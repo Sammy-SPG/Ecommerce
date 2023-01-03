077088a8a4edcbb3d28b333e63ed585ed7165f72
Sammy <Sandysuwu73@gmail.com>
2023-01-03 14:21:05 -0600

    inicia las variables de entorno

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { API_URL } from "../env";
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <h1>Hola {API_URL}</h1>
     </div>
   )
 }
